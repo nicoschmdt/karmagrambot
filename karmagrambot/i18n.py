5383126f0d0bd35f5df87035ebbaa8673ea1b0a7
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2022-07-27 22:23:07 -0300

    get locale defined in config.json

--- karmagrambot/i18n.py
+++ karmagrambot/i18n.py
@@ -1,5 +1,8 @@
 import gettext
+import locale
 from .config import LANGUAGE
+
+locale.setlocale(locale.LC_TIME, LANGUAGE)
 
 language = gettext.translation('base', localedir='locale', languages=[LANGUAGE]) # ver como pegar a linguagem do config
 language.install()
